6ed9e38c069d4922ce0fb3ac756ded120204e550
Oleg <olegfirst@ukr.net>
2024-11-29 16:45:32 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,51 +9,50 @@ import { getData } from './common/services';
 
 const App = () => {
   console.log();
-  
-	const [exchangeRateItems, setExchangeRateItems] = useState<IExchangeRate[]>([]);
-	
-	useEffect(() => {
-		getData()
-			.then((response: any) => {				
-				response?.json().then((data: IResponseData[]) => {					
-					const fullData = [
-						{
-							ccy: 'UAH',
-							base_ccy: '',
-							buy: '0',
-							sale: '0'
-						},
-						...data
-					];					
-					
-					setExchangeRateItems(
-						fullData.map((item: IResponseData, index: number) => ({
-							id: index + 1,
-							currency: item.ccy,
-							buy: Number(item.buy),
-							sale: Number(item.sale)
-						}))
-					);
-				});
-			})
-			.catch((err: any) => {
-				console.log('Error ', err);				
-			})
-	}, []);
-	
-	if (exchangeRateItems.length === 0) {
-		return (
-			<Load />
-		)
-	}
-	
-  return (
-  <section className='App'>
-			<Header exchangeRateItems={exchangeRateItems} />
-			
-  <Converter exchangeRateItems={exchangeRateItems} />
-  </section>
-  );
+const [exchangeRateItems, setExchangeRateItems] = useState<IExchangeRate[]>([]);
+
+useEffect(() => {
+getData()
+.then((response: any) => {				
+response?.json().then((data: IResponseData[]) => {					
+const fullData = [
+{
+ccy: 'UAH',
+base_ccy: '',
+buy: '0',
+sale: '0'
+},
+...data
+];					
+
+setExchangeRateItems(
+fullData.map((item: IResponseData, index: number) => ({
+id: index + 1,
+currency: item.ccy,
+buy: Number(item.buy),
+sale: Number(item.sale)
+}))
+);
+});
+})
+.catch((err: any) => {
+console.log('Error ', err);				
+})
+}, []);
+
+if (exchangeRateItems.length === 0) {
+return (
+<Load />
+)
+}
+
+return (
+<section className='App'>
+<Header exchangeRateItems={exchangeRateItems} />
+
+<Converter exchangeRateItems={exchangeRateItems} />
+</section>
+);
 }
 
 export default App;
