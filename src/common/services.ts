e9594d3430e80d0dcc0458c83643cad2f3e53c08
Oleg <olegfirst@ukr.net>
2024-11-30 19:50:15 +0200

    Code cleaning

--- src/common/services.ts
+++ src/common/services.ts
@@ -1,25 +1,25 @@
 import { serverName } from './constants';
 
 const errorHandler = (response: any) => {	
-	if (!response.ok) {
-		throw new Error(`HTTP error, status = ${response.status}`);
-	}
-	
-	return response;
+  if (!response.ok) {
+    throw new Error(`HTTP error, status = ${response.status}`);
+  }
+
+  return response;
 };
 
 /*
 * @var serverName link from current exchange rate is given
 */
 export async function getData() {
-	const response = await fetch(serverName, {
-		method: 'GET',
-		mode: 'no-cors',
-		cache: 'no-cache',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-	
-	return errorHandler(response);
+  const response = await fetch(serverName, {
+    method: 'GET',
+    mode: 'no-cors',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return errorHandler(response);
 };
